58b2539490e80874d0f5d1eeadc429e15de96b33
babakabdolahi <babak.abi86@gmail.com>
2025-01-30 14:02:07 +0330

    update list insert

--- list/list.go
+++ list/list.go
@@ -1,20 +1,60 @@
 // implementation of the list data structure based on the "Data Structures And Algorithms" by A. Aho eta.
 package list
 
-import "slices"
-
-const MaxListLength = 100
+import (
+	"fmt"
+	"slices"
+)
 
 type List struct {
 	Elements       []int
 	LastElPosition int
+	MaxLength      int
 }
 
-// inserts x into position p
+func New(maxLength int) List {
+	arr := make([]int, 0, maxLength)
+
+	return List{
+		Elements:       arr,
+		LastElPosition: 0,
+		MaxLength:      maxLength,
+	}
+}
+
+// Inserts x into position p.
 func (list *List) Insert(x, p int) error {
+	if list.Elements == nil {
+		return fmt.Errorf("nil list")
+	}
+
+	if list.isPositionNegative(p) {
+		return fmt.Errorf("negative position")
+	}
+
+	if list.isListFull() {
+		return fmt.Errorf("list is full")
+	}
+
+	if list.isPositionOutOfRange(p) {
+		return fmt.Errorf("position is out of range")
+	}
+
 	list.Elements = slices.Insert(list.Elements, p, x)
 
 	list.LastElPosition = len(list.Elements) - 1
 
 	return nil
+}
+
+func (list *List) isPositionNegative(p int) bool {
+	return p < 0
+}
+
+func (list *List) isListFull() bool {
+	return list.LastElPosition == list.MaxLength-1
+}
+
+func (list *List) isPositionOutOfRange(p int) bool {
+	return p > list.LastElPosition+1
 }
--- list/list_test.go
+++ list/list_test.go
@@ -4,12 +4,24 @@ import "testing"
 
 // insert an intermediate element
 func TestListInsert1(t *testing.T) {
-	list := List{
-		Elements:       []int{1, 4, 6},
-		LastElPosition: 2,
+	list := New(10)
+
+	err := list.Insert(1, 0)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	err := list.Insert(10, 1)
+	err = list.Insert(2, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = list.Insert(3, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = list.Insert(4, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,11 +30,124 @@ func TestListInsert1(t *testing.T) {
 		t.Errorf("expected list length: %v\n got list length: %v", 4, len(list.Elements))
 	}
 
-	if list.Elements[0] != 1 || list.Elements[1] != 10 || list.Elements[2] != 4 || list.Elements[3] != 6 {
-		t.Fatal("incorrect insertion")
+	if list.Elements[0] != 1 || list.Elements[1] != 4 || list.Elements[2] != 2 || list.Elements[3] != 3 {
+		t.Error("incorrect insertion")
 	}
 
 	if list.LastElPosition != 3 {
 		t.Errorf("expected last element position: %v\n got last element position: %v", 3, list.LastElPosition)
+	}
+}
+
+// insert into nil list
+func TestListInsert2(t *testing.T) {
+	list := List{
+		Elements: nil,
+	}
+
+	err := list.Insert(10, 1)
+	if err != nil {
+		expectedErr := "nil list"
+
+		if err.Error() != expectedErr {
+			t.Errorf("wrong error, expected %v\n got %v", expectedErr, err.Error())
+		}
+	} else {
+		t.Error("the purpose of the test is not met")
+	}
+}
+
+// list max length limit
+func TestListInsert3(t *testing.T) {
+	list := New(2)
+
+	err := list.Insert(1, 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = list.Insert(2, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = list.Insert(3, 2)
+	if err != nil {
+		expectedErr := "list is full"
+
+		if err.Error() != expectedErr {
+			t.Errorf("wrong error, expected %v\n got %v", expectedErr, err.Error())
+		}
+	} else {
+		t.Error("the purpose of the test is not met")
+	}
+}
+
+// list max length limit
+func TestListInsert4(t *testing.T) {
+	list := New(2)
+
+	err := list.Insert(1, 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = list.Insert(2, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = list.Insert(3, 2)
+	if err != nil {
+		expectedErr := "list is full"
+
+		if err.Error() != expectedErr {
+			t.Errorf("wrong error, expected %v\n got %v", expectedErr, err.Error())
+		}
+	} else {
+		t.Error("the purpose of the test is not met")
+	}
+}
+
+// out of range position
+func TestListInsert5(t *testing.T) {
+	list := New(3)
+
+	err := list.Insert(1, 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = list.Insert(2, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// instead of position 2, insert into position 3
+	err = list.Insert(3, 3)
+	if err != nil {
+		expectedErr := "position is out of range"
+
+		if err.Error() != expectedErr {
+			t.Errorf("wrong error, expected %v\n got %v", expectedErr, err.Error())
+		}
+	} else {
+		t.Error("the purpose of the test is not met")
+	}
+}
+
+// negative position
+func TestListInsert6(t *testing.T) {
+	list := New(1)
+
+	err := list.Insert(1, -1)
+	if err != nil {
+		expectedErr := "negative position"
+
+		if err.Error() != expectedErr {
+			t.Errorf("wrong error, expected %v\n got %v", expectedErr, err.Error())
+		}
+	} else {
+		t.Error("the purpose of the test is not met")
 	}
 }
